efc980397ce58a374826f3cd84eff9eb7ef2fc0f
Gerardo <gerardo_orellana1996@hotmail.es>
2019-08-02 01:57:43 -0400

    check project

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pins
   get 'pages/index'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'pages#index'
+  get 'mypins' => 'pins#mypins'
+#get 'pinsof/:user_id' => 'pins#pinsof', :as => 'pinsof'
 end
